21a39a8812afb866690c695b8cc580dd9f9d8f7b
Jose <jose.antonio.umeres@gmail.com>
2022-08-30 09:48:09 -0500

    Added LazyPage components

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
+import { LazyPage1, LazyPage2, LazyPage3 } from "../lazyload/pages";
 import logo from '../logo.svg';
 
 export const Navigation = () => {
@@ -9,23 +10,30 @@ export const Navigation = () => {
                     <img src={ logo } alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/lazy1" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage1</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                            <NavLink to="/lazy2" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage2</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                            <NavLink to="/lazy3" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage3</NavLink>
                         </li>
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="about" element={<h1>About Page</h1>}/>
-                    <Route path="users" element={<h1>User Page</h1>}/>
-                    <Route path="/home" element={<h1>Home Page</h1>}/>
+                    <Route path="lazy1" element={<LazyPage1 />}/>
+                    <Route path="lazy2" element={<LazyPage2 />}/>
+                    <Route path="lazy3" element={<LazyPage3 />}/>
                     {/* con el replace se reemplaza el historial y ya no puede hacer back de pagina */}
-                    <Route path="/*" element={<Navigate to="/home" replace/>}/>
+                    <Route path="/*" element={<Navigate to="lazy1" replace/>}/>
                 </Routes>
+                {/* <Routes> */}
+                    {/* <Route path="about" element={<h1>About Page</h1>}/> */}
+                    {/* <Route path="users" element={<h1>User Page</h1>}/> */}
+                    {/* <Route path="/home" element={<h1>Home Page</h1>}/> */}
+                    {/* con el replace se reemplaza el historial y ya no puede hacer back de pagina */}
+                    {/* <Route path="/*" element={<Navigate to="/home" replace/>}/> */}
+                {/* </Routes>  */}
             </div>
         </BrowserRouter>
     )
