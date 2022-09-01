7985b6577b88c8aef4efc99420ec22cf19345b4c
Jose <jose.antonio.umeres@gmail.com>
2022-08-31 20:42:47 -0500

    Added routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
-import { LazyPage1, LazyPage2, LazyPage3 } from "../lazyload/pages";
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 export const Navigation = () => {
     return (
@@ -9,23 +9,30 @@ export const Navigation = () => {
                 <nav>
                     <img src={ logo } alt="React Logo" />
                     <ul>
-                        <li>
-                            <NavLink to="/lazy1" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage1</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/lazy2" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage2</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/lazy3" className={({ isActive }) => isActive ? 'nav-active' : '' }>LazyPage3</NavLink>
-                        </li>
+                        {
+                            routes.map(({ to, name }) => (
+                                <li key={to}>
+                                    <NavLink
+                                        to={to}
+                                        className={({ isActive }) => isActive ? 'nav-active' : '' }>
+                                        {name}
+                                    </NavLink>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="lazy1" element={<LazyPage1 />}/>
-                    <Route path="lazy2" element={<LazyPage2 />}/>
-                    <Route path="lazy3" element={<LazyPage3 />}/>
-                    {/* con el replace se reemplaza el historial y ya no puede hacer back de pagina */}
-                    <Route path="/*" element={<Navigate to="lazy1" replace/>}/>
+                    {
+                        routes.map(({path, Component}) => (
+                            <Route
+                                key={path}
+                                path={path}
+                                element={<Component />}
+                            />
+                        ))
+                    }
+                    <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
                 </Routes>
                 {/* <Routes> */}
                     {/* <Route path="about" element={<h1>About Page</h1>}/> */}
