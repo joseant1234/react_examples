aa17680a3c144db5efa8117f007595cdd28454ac
Jose <jose.antonio.umeres@gmail.com>
2022-09-03 09:47:11 -0500

    Removed the lazy load section

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,53 +1,37 @@
-import { Suspense } from "react";
-import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
-import logo from '../logo.svg';
-import { routes } from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
-// con el suspense sirve para decirle al react q cuando se esta cargando un componente, módulo que espere, mientras este cargando que haga una accion como reendirzar un componente (en fallback)
-// si está el fallback en null no reenderizará ningun componente
+import logo from '../logo.svg'
+
 export const Navigation = () => {
     return (
-        <Suspense fallback={<span>Loading...</span>}>
-            <BrowserRouter>
-                <div className="main-layout">
-                    <nav>
-                        <img src={ logo } alt="React Logo" />
-                        <ul>
-                            {
-                                routes.map(({ to, name }) => (
-                                    <li key={to}>
-                                        <NavLink
-                                            to={to}
-                                            className={({ isActive }) => isActive ? 'nav-active' : '' }>
-                                            {name}
-                                        </NavLink>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </nav>
-                    <Routes>
-                        {
-                            routes.map(({path, Component}) => (
-                                <Route
-                                    key={path}
-                                    path={path}
-                                    element={<Component />}
-                                />
-                            ))
-                        }
-                        <Route path="/*" element={<Navigate to={routes[0].to} replace/>}/>
-                    </Routes>
-                    {/* <Routes> */}
-                        {/* <Route path="about" element={<h1>About Page</h1>}/> */}
-                        {/* <Route path="users" element={<h1>User Page</h1>}/> */}
-                        {/* <Route path="/home" element={<h1>Home Page</h1>}/> */}
-                        {/* con el replace se reemplaza el historial y ya no puede hacer back de pagina */}
-                        {/* <Route path="/*" element={<Navigate to="/home" replace/>}/> */}
-                    {/* </Routes>  */}
-                </div>
-            </BrowserRouter>
-        </Suspense>
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={ logo } alt="React Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
 
+
+                <Routes>
+                    <Route path="about" element={ <h1>About Page</h1> } />
+                    <Route path="users" element={ <h1>Users Page</h1> } />
+                    <Route path="home" element={ <h1>Home Page</h1> } />
+
+                    <Route path="/*" element={ <Navigate to="/home" replace /> } />
+                </Routes>
+
+            </div>
+        </BrowserRouter>
     )
 }
