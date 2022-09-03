6dd9630c1813fcef7bf0edd314e5f649d26677f5
Jose <jose.antonio.umeres@gmail.com>
2022-09-03 09:39:04 -0500

    Removed unnecessary comments

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,33 +10,7 @@ interface Route {
     name: string;
 }
 
-// const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../lazyload/pages/LazyPage1'));
-// const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../lazyload/pages/LazyPage2'));
-// const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../lazyload/pages/LazyPage3'));
-
 const LazyLayout = lazy(() => import(/* webpackChunkName: "LazyLayout" */ '../lazyload/layout/LazyLayout'));
-
-// el path para cargar el componente
-// export const routes: Route[] = [
-//     {
-//         to: '/lazy1',
-//         path: 'lazy1',
-//         Component: Lazy1,
-//         name: 'Lazy-1'
-//     },
-//     {
-//         to: '/lazy2',
-//         path: 'lazy2',
-//         Component: Lazy2,
-//         name: 'Lazy-2'
-//     },
-//     {
-//         to: '/lazy3',
-//         path: 'lazy3',
-//         Component: Lazy3,
-//         name: 'Lazy-3'
-//     }
-// ];
 
 // todo lo q venga luego /lazyload sera procesado por el router q se definirá en el LazyLayout
 export const routes: Route[] = [
