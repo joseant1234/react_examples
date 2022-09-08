20a7c53d06457aabfce119a1877c266972170ba0
Jose <jose.antonio.umeres@gmail.com>
2022-09-07 20:48:18 -0500

    Added props to Product card component

--- src/component-patterns/components/ProductCard.tsx
+++ src/component-patterns/components/ProductCard.tsx
@@ -1,17 +1,25 @@
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
-import { useState } from 'react';
 import { useProduct } from '../hooks/useProduct';
 
+interface Props {
+    product: Product
+}
+interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
 // al hacerlo como modulo esos estilos son unicos, los está encapsulando
-export const ProductCard = () => {
+export const ProductCard = ({ product }: Props) => {
     const { counter, increaseBy } = useProduct();
 
     return (
         <div className={styles.productCard}>
-            <img className={styles.productImg} src="./coffee-mug.png" alt="Coffee Mug" />
+            <img className={styles.productImg} src={product.img ? product.img : noImage } alt="Coffee Mug" />
             {/* <img className={styles.productImg} src={noImage} alt="Coffee Mug" /> */}
-            <span className={styles.productDescription}>Coffee Mug</span>
+            <span className={styles.productDescription}>{ product.title }</span>
             <div className={styles.buttonsContainer}>
                 <button
                     className={styles.buttonMinus}
