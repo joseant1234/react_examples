27e6b7140c6328e28c46698fbba1e506b3ce43e0
Jose <jose.antonio.umeres@gmail.com>
2022-09-24 08:31:36 -0500

    Control props alt finished

--- src/component-patterns/hooks/useProduct.ts
+++ src/component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/common";
 
 interface useProductArgs {
@@ -9,16 +9,8 @@ interface useProductArgs {
 
 export const useProduct = ({ onChange, product, value  = 0 }: useProductArgs) => {
     const [counter, setCounter] = useState(value);
-    // para no redibujar los componentes en caso de que cambie
-    // si existe la funcion onChange, significa que va ser controlado desde afuera, y por tanto solo necesita que se le envie la cantidad cambiada (+1, -1)
-    // por tanto el setCounter se manejaría en caso no se enví una función de onChange
-    const isControlled = useRef(!!onChange);
 
     const increaseBy = (value: number) => {
-        if (isControlled.current) {
-            // ! es para obligar a ts que si ignore el posible error
-            return onChange!({ count: value, product });
-        }
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue);
         // setCounter(prev => Math.max(prev + value, 0));
--- src/component-patterns/hooks/useShoppingCart.ts
+++ src/component-patterns/hooks/useShoppingCart.ts
@@ -5,35 +5,19 @@ import { Product, ProductInCart } from "../interfaces/common";
 export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
+    // aqui el count recibe el valor ya calculado del producto
     const onProductCountChange = ({ count, product }: { count: number, product: Product}) => {
 
         setShoppingCart(oldShoppingCart => {
-
-          // si es nulo significa que no existe, entonces se crea el produto en base al recibido
-          const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-          // si es mayor que 0 significa que se puede incrementar en la cantidad (+1 o -1)
-          if (Math.max(productInCart.count  + count, 0) > 0) {
-            productInCart.count += count;
-            return {
-              ...oldShoppingCart,
-              [product.id]: productInCart,
-            }
+          if (count === 0) {
+            const { [product.id]:toDelete, ...rest } = oldShoppingCart;
+            return rest;
           }
-          // significa que hay q borrar el producto, porque la sumatorio es 0 o menor (-1)
-          const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-          return rest;
 
-          // otra forma de hacerlo
-          // if (count === 0) {
-          //   const { [product.id]:toDelete, ...rest } = oldShoppingCart;
-          //   return rest;
-          // }
-
-          // return {
-          //   ...oldShoppingCart,
-          //   [product.id]: {...product, count}
-          // };
+          return {
+            ...oldShoppingCart,
+            [product.id]: {...product, count}
+          };
         });
       }
 
