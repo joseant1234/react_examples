4a701adbb8dc0f266234c5d21779c72fd20d2a9a
Jose <jose.antonio.umeres@gmail.com>
2022-10-16 21:28:34 -0500

    Added validations

--- src/forms/pages/RegisterPage.tsx
+++ src/forms/pages/RegisterPage.tsx
@@ -1,26 +1,23 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+ import { FormEvent } from 'react';
+import { useForm } from '../hooks/useForm';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
-    const [registerData, setRegisterData] = useState({
+
+    const {
+        formData, onChange, resetForm, isValidEmail,
+        name, email, password1, password2,
+    } = useForm({
         name: '',
         email: '',
         password1: '',
         password2: '',
     });
 
-    const { name, email, password1, password2 } = registerData;
-
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setRegisterData(prev => ({
-            ...prev,
-            [event.target.name]: event.target.value
-        }));
-    }
 
     const onSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(registerData)
+        console.log(formData)
     }
 
     return (
@@ -33,14 +30,19 @@ export const RegisterPage = () => {
                     name="name"
                     value={ name }
                     onChange={ onChange }
+                    className={`${name.trim().length <= 0 && 'has-error'}`}
                 />
+                { name.trim().length <= 0 && <span>Este campo es necesario</span> }
                 <input
                     type="email"
                     placeholder="Email"
                     name="email"
                     value={ email }
                     onChange={ onChange }
+                    className={`${!isValidEmail(email) && 'has-error'}`}
                 />
+                { !isValidEmail(email) && <span>Email no es válido</span> }
+
                 <input
                     type="password"
                     placeholder="Password"
@@ -48,6 +50,8 @@ export const RegisterPage = () => {
                     value={ password1 }
                     onChange={ onChange }
                 />
+                { password1.trim().length <= 0 && <span>Este campo es necesario</span> }
+                { password1.trim().length < 6 && password1.trim().length > 0  && <span>La contraseña tiene que tener 6 letras</span> }
                 <input
                     type="password"
                     placeholder="Repeat password"
@@ -55,7 +59,10 @@ export const RegisterPage = () => {
                     value={ password2 }
                     onChange={ onChange }
                 />
+                { password2.trim().length <= 0 && <span>Este campo es necesario</span> }
+                { password2.trim().length > 0 && password1 !== password2 && <span>Las contraseñas deben ser iguales</span> }
                 <button type="submit">Create</button>
+                <button type="button" onClick={ resetForm }>Reset Form</button>
             </form>
         </div>
     )
