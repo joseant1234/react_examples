416583a649303c45579c3cbbb8bdf01ceccfabe5
Jose <jose.antonio.umeres@gmail.com>
2022-10-19 20:51:41 -0500

    Added validate function

--- src/forms/pages/FormikBasicPage.tsx
+++ src/forms/pages/FormikBasicPage.tsx
@@ -1,34 +1,86 @@
+import { FormikErrors, useFormik } from 'formik';
 import '../styles/styles.css';
 
+interface FormValues {
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
 export const FormikBasicPage = () => {
-  return (
-    <div>
-        <h1>Formik Basic</h1>
-        <form noValidate>
-            <label htmlFor='firstName'>First name</label>
-            <input
-                type="text"
-                name="firstName"
-            />
-            <span>First name is required</span>
 
-            <label htmlFor='lastName'>Last name</label>
-            <input
-                type="text"
-                name="lastName"
-            />
-            <span>Last name is required</span>
+    const validate = ({ firstName, lastName, email }: FormValues) => {
+        const errors: FormikErrors<FormValues> = {};
+        if (!firstName) {
+            errors.firstName = 'Required';
+        } else if (firstName.length >= 15) {
+            errors.firstName = 'Must be 15 characters or less';
+        }
 
-            <label htmlFor='email'>Email</label>
-            <input
-                type="email"
-                name="email"
-            />
-            <span>Email is required</span>
-            <span>Check for a valid email format</span>
+        if (!lastName) {
+            errors.lastName = 'Required';
+        } else if (firstName.length >= 10) {
+            errors.lastName = 'Must be 10 characters or less';
+        }
 
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-  )
+        if (!email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        return errors;
+    }
+
+    // onsubmit se ejecutará cuando el formulario pase todas las reglas de validación
+    const { handleChange, values, handleSubmit, errors, touched, handleBlur } = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        onSubmit: (values) => {
+            console.log(values)
+        },
+        validate,
+    });
+    // lo que esté en la prop name tiene coincidir con lo que esté values.<prop>
+    return (
+        <div>
+            <h1>Formik Basic</h1>
+            <form onSubmit={ handleSubmit } noValidate>
+                <label htmlFor='firstName'>First name</label>
+                <input
+                    type="text"
+                    name="firstName"
+                    onBlur={ handleBlur }
+                    onChange={ handleChange }
+                    value={ values.firstName }
+                />
+                { touched.firstName && errors.firstName && <span>{ errors.firstName }</span> }
+
+                <label htmlFor='lastName'>Last name</label>
+                <input
+                    type="text"
+                    name="lastName"
+                    onBlur={ handleBlur }
+                    onChange={ handleChange }
+                    value={ values.lastName }
+                />
+                { touched.lastName && errors.lastName && <span>{ errors.lastName }</span> }
+
+                <label htmlFor='email'>Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    onBlur={ handleBlur }
+                    onChange={ handleChange }
+                    value={ values.email }
+                />
+                { touched.email && errors.email && <span>{ errors.email }</span> }
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
